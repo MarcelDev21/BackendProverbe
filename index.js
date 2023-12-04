324165c002828f9da385c1d873277b51e87d532c
MarcelDev <marceldev96@gmail.com>
2023-12-05 00:19:28 +0100

    tft

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ app.use(express.json({limit: '100mb'}))
 app.use(express.urlencoded({limit: '100mb', extended: true}))
 
 app.use('/api', ProverbeRoute)
+app.use('/', (req,res)=> {
+    res.status(200).send("connected successfuly")
+})
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(process.env.Port || port, () => console.log(`Example app listening on port ${process.env.Port}!`))
